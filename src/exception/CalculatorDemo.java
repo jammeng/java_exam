17472ae1a50e7ddb2c49bcb1bf55ea1af911b47b
kjmyeong <cptm2399@gmail.com>
2021-05-18 23:44:29 +0900

    21-05-18 java

--- src/exception/CalculatorDemo.java
+++ src/exception/CalculatorDemo.java
@@ -1,33 +1,34 @@
 package exception;
 
+class DivideException extends Exception {
+    DivideException(){
+        super();
+    }
+    DivideException(String message){
+        super(message);
+    }
+}
 class Calculator{
     int left, right;
-    public void setOprands(int left, int right){
+    public void setOprands(int left, int right){        
         this.left = left;
         this.right = right;
     }
-    public void divide(){
-        try {
-            System.out.print("계산결과는 ");
-            System.out.print(this.left/this.right);
-            System.out.print(" 입니다.");
-        } catch(Exception e){
-//            System.out.println("오류가 발생했습니다 : "+e.getMessage());
-        	System.out.println("\n\ne.getMessage()\n"+e.getMessage());
-            System.out.println("\n\ne.toString()\n"+e.toString());
-            System.out.println("\n\ne.printStackTrace()");
-            e.printStackTrace();
+    public void divide() throws DivideException{
+        if(this.right == 0){
+            throw new DivideException("0으로 나누는 것은 허용되지 않습니다.");
         }
+        System.out.print(this.left/this.right);
     }
-} 
+}
 public class CalculatorDemo {
     public static void main(String[] args) {
         Calculator c1 = new Calculator();
         c1.setOprands(10, 0);
-        c1.divide();
-         
-        Calculator c2 = new Calculator();
-        c2.setOprands(10, 5);
-        c2.divide();
+        try {
+            c1.divide();
+        } catch (DivideException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/exception/CheckedExceptionDemo.java
@@ -0,0 +1,24 @@
+package exception;
+import java.io.*;
+
+public class CheckedExceptionDemo {
+
+	public static void main(String[] args) {
+		BufferedReader bReader = null;
+		try {
+			bReader = new BufferedReader(new FileReader("out.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String input = null;
+		try {
+			input = bReader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(input);	
+	}
+
+}
